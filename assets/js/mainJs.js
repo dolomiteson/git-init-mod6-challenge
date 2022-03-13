217597fb631d2f35f9340655fea736dbf8b79e77
Dolo_Mane <zimmerman.lucas@hotmail.com>
2022-03-12 18:26:43 -0600

    Update to html to address div size for current weather

--- assets/js/mainJs.js
+++ assets/js/mainJs.js
@@ -1,2 +1,3 @@
 /* City Data Funtionality */
 // TODO: Function that will generate City button upon search result
+// Example of format will be: <button type="button" class="w-100 bg-secondary my-2">Search</button>
