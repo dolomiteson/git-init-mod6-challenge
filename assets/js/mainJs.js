0c43f26ef13e0715110cc57807e3a376046f8b0e
Dolo_Mane <zimmerman.lucas@hotmail.com>
2022-03-12 19:14:56 -0600

    Update to JS file to make initial API cal

--- assets/js/mainJs.js
+++ assets/js/mainJs.js
@@ -1,3 +1,12 @@
 /* City Data Funtionality */
 // TODO: Function that will generate City button upon search result
 // Example of format will be: <button type="button" class="w-100 bg-secondary my-2">Search</button>
+// Fetch API response to get results
+var requestUrl = 'https://api.openweathermap.org/data/2.5/onecall?lat=33.44&lon=-94.04&exclude=minutely,hourly&appid=9bb87156295234b5818a2a3fbabeb8d1';
+fetch(requestUrl)
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(data) {
+        console.log(data);
+    });
