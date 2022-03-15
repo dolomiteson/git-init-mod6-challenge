1a5c7efd7fc5bc48e1af9b96a631ba2499e3c0b1
Dolo_Mane <zimmerman.lucas@hotmail.com>
2022-03-15 12:45:34 -0500

    Update to JS file concerning API calls

--- assets/js/mainJs.js
+++ assets/js/mainJs.js
@@ -1,12 +1,23 @@
-/* City Data Funtionality */
+/* Variables */
+var apiKey = '9bb87156295234b5818a2a3fbabeb8d1'; 
+var getCityInfo = 'http://api.openweathermap.org/geo/1.0/direct?q=St. Louis,Missouri,US&limit=5&appid=' + apiKey;
+var getWeather = 'https://api.openweathermap.org/data/2.5/onecall?lat=39.7990175&lon=-89.6439575&exclude=minutely,hourly&appid=' + apiKey;
+getResponse(getWeather);
+getResponse(getCityInfo);
+
+/* City Data 
+Funtionality */
 // TODO: Function that will generate City button upon search result
 // Example of format will be: <button type="button" class="w-100 bg-secondary my-2">Search</button>
 // Fetch API response to get results
-var requestUrl = 'https://api.openweathermap.org/data/2.5/onecall?lat=33.44&lon=-94.04&exclude=minutely,hourly&appid=9bb87156295234b5818a2a3fbabeb8d1';
-fetch(requestUrl)
-    .then(function(response) {
-      return response.json();
-    })
-    .then(function(data) {
-        console.log(data);
-    });
+
+// Function to get responses from API
+function getResponse(requestUrl){
+  fetch(requestUrl)
+      .then(function(response) {
+        return response.json();
+      })
+      .then(function(data) {
+          console.log(data);
+      });
+}
