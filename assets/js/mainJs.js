3350586b1814f7c71803cc7983f0cf8b1d3ddd54
Dolo_Mane <zimmerman.lucas@hotmail.com>
2022-03-16 16:08:38 -0500

    Update to js: Add functionality to retrieve city data and store in local

--- assets/js/mainJs.js
+++ assets/js/mainJs.js
@@ -1,23 +1,49 @@
 /* Variables */
 const API_KEY = '9bb87156295234b5818a2a3fbabeb8d1'; 
-var getCityInfo = 'http://api.openweathermap.org/geo/1.0/direct?q=DC,US&appid=' + API_KEY;
-var getWeather = 'https://api.openweathermap.org/data/2.5/onecall?lat=39.7990175&lon=-89.6439575&exclude=minutely,hourly&appid=' + API_KEY;
-getResponse(getWeather);
-getResponse(getCityInfo);
+const CITY_STORE = 'cityStore'
 
-/* City Data 
-Funtionality */
-// TODO: Function that will generate City button upon search result
-// Example of format will be: <button type="button" class="w-100 bg-secondary my-2">Search</button>
-// Fetch API response to get results
+// City Infor variables
+var getCityUrl = '';
+var cityInfo = '';
 
-// Function to get responses from API
-function getResponse(requestUrl){
-  fetch(requestUrl)
-      .then(function(response) {
-        return response.json();
-      })
-      .then(function(data) {
-          console.log(data);
-      });
+var weatherInfo = 'https://api.openweathermap.org/data/2.5/onecall?lat=39.7990175&lon=-89.6439575&exclude=minutely,hourly&appid=' + API_KEY;
+
+
+/* City Data Funtionality */
+
+// Function to build getCityInfo request
+async function getCityInfo(cityVal, stateVal){
+  getCityUrl = 'http://api.openweathermap.org/geo/1.0/direct?q=' + cityVal + ',' + stateVal + ',US&appid=' + API_KEY;
+
+  const response = await fetch(getCityUrl);
+  const data = await response.json();
+  const {name, state, lat, lon} = data[0];
+
+  var cities = JSON.parse(localStorage.getItem(CITY_STORE)) ?? [];
+
+  // Remove any old records to be replaced
+  for(var index = 0; index < cities.length; index++){
+    if(cities[index].name === name && cities[index].state === state){
+        cities.splice(index, 1);
+    }
+  }
+
+  cities.push({name, state, lat, lon});
+
+  cities.sort(function(a, b){
+    var textA = a.name.toUpperCase();
+    var textB = b.name.toUpperCase();
+    return(textA < textB) ? -1 : (textA > textB) ? 1 : 0;
+  });
+
+  // Set local storage
+  localStorage.setItem(CITY_STORE, JSON.stringify(cities));
 }
+
+// Function that displays cities stored in local storage
+function createCityButtons(){
+  // Example of format will be: <button type="button" class="w-100 bg-secondary my-2">Search</button>
+}
+
+getCityInfo("Virginia Beach", "Virginia");
+
